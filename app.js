31aceb168044a6e062240853b551193ea6818bf5
Naman Dharmani <namandharmani@gmail.com>
2022-05-26 18:38:13 +0530

    Added running python file in child process if exists

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const djs = require("dayjs");
 
 const path = require("path");
 const fs = require("fs");
-// const { spawn } = require("child_process");
+const { spawn } = require("child_process");
 
 const app = express();
 const port = 5000;
 const uploadsDirectory = path.join(__dirname, "uploads");
 
 let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, "uploads"); // Directory to store all uploads
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     // File name to be stored: <fileName>-<timestamp>
     let fileName = path.parse(file.originalname)
     cb(null, fileName.name + "-" + djs().format("DDMMYYHHmmss") + fileName.ext);
@@ -26,7 +26,7 @@ const maxSize = 1 * 1000 * 1000; // 1 MB
 let upload = multer({
   storage: storage,
   limits: { fileSize: maxSize },
-  fileFilter: function (req, file, cb) {
+  fileFilter: (req, file, cb) => {
     let filetypes = /py/;
     let mimetype = filetypes.test(file.mimetype);
     console.log(file.mimetype);
@@ -44,28 +44,43 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.get("/", (req, res) => {
-  fs.readdir(uploadsDirectory, function (err, files) {
+  fs.readdir(uploadsDirectory, (err, files) => {
     if (err) {
       return console.log('Unable to scan directory: ' + err);
     }
-
     res.render("index", { files });
   });
 
 });
 
 app.post("/upload/", (req, res) => {
-  upload(req, res, function (err) {
+  upload(req, res, (err) => {
     if (err) {
       res.send(err);
     }
-
     res.render("success", { file: req.file.filename });
   });
 });
 
-app.get("/run/:file", (req, res) => {
-  res.send("File successfully run");
+app.get("/run/:fileName", (req, res) => {
+  const { base: fileName, ext: extName } = path.parse(req.params.fileName)
+  // console.log(fileName, extName)
+  fs.exists(`uploads/${fileName}`, (isExists) => {
+    if(isExists && extName.toLowerCase() == '.py') {
+      let dataToSend;
+      const python = spawn('python', [`uploads/${fileName}`]);
+      python.stdout.on('data', function (data) {
+        dataToSend = data.toString();
+      });
+      python.on('close', (code) => {
+        res.write(dataToSend);
+        res.end();
+      });
+    } 
+    else {
+      res.status(404).send("File does not exists or is not a python script!");
+    }
+  })
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
